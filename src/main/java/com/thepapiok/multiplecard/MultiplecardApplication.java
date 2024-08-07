34b1d5fd0a653b1b99d451fc827573504fe069a8
Adrian Politowski <politowskiadrian78@gmail.com>
2024-08-07 14:56:04 +0200

    Initalize project

---
+++ src/main/java/com/thepapiok/multiplecard/MultiplecardApplication.java
@@ -0,0 +1,13 @@
+package com.thepapiok.multiplecard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiplecardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiplecardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thepapiok/multiplecard/MultiplecardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thepapiok.multiplecard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiplecardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
