00769ffd9787ef4bbb7af6d7682bd102350b196b
Adrian Politowski <politowskiadrian78@gmail.com>
2024-08-08 11:56:36 +0200

    Back to correct code

--- src/main/java/com/thepapiok/multiplecard/MultiplecardApplication.java
+++ src/main/java/com/thepapiok/multiplecard/MultiplecardApplication.java
@@ -3,7 +3,6 @@ package com.thepapiok.multiplecard;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.*;
 
 @SpringBootApplication
 public class MultiplecardApplication {
