1194f49c6acdc01f75363f410b10ce0aef799fa6
Adrian Politowski <politowskiadrian78@gmail.com>
2024-08-19 12:54:04 +0200

    Add AccountRepository

---
+++ src/main/java/com/thepapiok/multiplecard/repositories/AccountRepository.java
@@ -0,0 +1,7 @@
+package com.thepapiok.multiplecard.repositories;
+
+import com.thepapiok.multiplecard.collections.Account;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface AccountRepository extends MongoRepository<Account, String> {
+}
