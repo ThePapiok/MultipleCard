adb2f2b666ec9d29196961e2f0c1adf2124fe18d
Adrian Politowski <politowskiadrian78@gmail.com>
2024-08-21 07:20:58 +0200

    Fix test

--- src/main/java/com/thepapiok/multiplecard/services/SmsService.java
+++ src/main/java/com/thepapiok/multiplecard/services/SmsService.java
@@ -4,21 +4,27 @@ import com.twilio.Twilio;
 import com.twilio.rest.api.v2010.account.Message;
 import com.twilio.type.PhoneNumber;
 import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SmsService {
 
-  private static final String ACCOUNT_SID = System.getenv("ACCOUNT_SID");
-  private static final String AUTH_TOKEN = System.getenv("AUTH_TOKEN");
-  private static final String PHONE_NUMBER = System.getenv("PHONE_NUMBER");
+  @Value("${ACCOUNT_SID}")
+  private String accountSid;
+
+  @Value("${AUTH_TOKEN}")
+  private String authToken;
+
+  @Value("${PHONE_NUMBER}")
+  private String phoneNumber;
 
   @PostConstruct
   public void init() {
-    Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
+    Twilio.init(accountSid, authToken);
   }
 
   public void sendSms(String text, String number) {
-    Message.creator(new PhoneNumber(number), new PhoneNumber(PHONE_NUMBER), text).create();
+    Message.creator(new PhoneNumber(number), new PhoneNumber(phoneNumber), text).create();
   }
 }
--- src/test/java/com/thepapiok/multiplecard/MultiplecardApplicationTests.java
+++ src/test/java/com/thepapiok/multiplecard/MultiplecardApplicationTests.java
@@ -2,8 +2,10 @@ package com.thepapiok.multiplecard;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class MultiplecardApplicationTests {
 
   @Test
