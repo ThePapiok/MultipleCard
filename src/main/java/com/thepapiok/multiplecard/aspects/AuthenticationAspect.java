324f4b44ec050f7ae0a6a971d852b03efb22e87a
Adrian Politowski <politowskiadrian78@gmail.com>
2024-08-22 08:15:01 +0200

    Remove whitespace from phone

--- src/main/java/com/thepapiok/multiplecard/aspects/AuthenticationAspect.java
+++ src/main/java/com/thepapiok/multiplecard/aspects/AuthenticationAspect.java
@@ -2,6 +2,8 @@ package com.thepapiok.multiplecard.aspects;
 
 import com.thepapiok.multiplecard.dto.RegisterDTO;
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -15,5 +17,13 @@ public class AuthenticationAspect {
   public void removeWhiteSpaceRegister(JoinPoint joinPoint) {
     RegisterDTO registerDTO = (RegisterDTO) joinPoint.getArgs()[0];
     registerDTO.setPhone(registerDTO.getPhone().replaceAll(" ", ""));
+  }
+
+  @Around("execution(* com.thepapiok.multiplecard.services.UserService.loadUserByUsername(..))")
+  public Object removeWhiteSpaceLogin(ProceedingJoinPoint joinPoint) throws Throwable {
+    Object[] args = joinPoint.getArgs();
+    String phone = (String) args[0];
+    args[0] = phone.replaceAll(" ", "");
+    return joinPoint.proceed(args);
   }
 }
