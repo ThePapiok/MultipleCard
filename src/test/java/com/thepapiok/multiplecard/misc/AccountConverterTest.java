479b2ef35ccd089fb2fd742a4c9e56dbd99ee3f8
Adrian Politowski <politowskiadrian78@gmail.com>
2024-08-26 09:02:19 +0200

    Add misc tests

---
+++ src/test/java/com/thepapiok/multiplecard/misc/AccountConverterTest.java
@@ -0,0 +1,40 @@
+package com.thepapiok.multiplecard.misc;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+import com.thepapiok.multiplecard.collections.Account;
+import com.thepapiok.multiplecard.dto.RegisterDTO;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+public class AccountConverterTest {
+  private AccountConverter accountConverter;
+  @Mock private PasswordEncoder passwordEncoder;
+
+  @BeforeEach
+  public void setUp() {
+    MockitoAnnotations.openMocks(this);
+    accountConverter = new AccountConverter(passwordEncoder);
+  }
+
+  @Test
+  public void shouldSuccessGetEntity() {
+    final String password = "Test123!";
+    final String encodePassword = "123dsfavdva3312";
+    RegisterDTO registerDTO = new RegisterDTO();
+    registerDTO.setPassword(password);
+    registerDTO.setCallingCode("+4");
+    registerDTO.setPhone("8");
+    Account expectedAccount = new Account();
+    expectedAccount.setPassword(encodePassword);
+    expectedAccount.setPhone("+48");
+
+    when(passwordEncoder.encode(password)).thenReturn(encodePassword);
+
+    assertEquals(expectedAccount, accountConverter.getEntity(registerDTO));
+  }
+}
---
+++ src/test/java/com/thepapiok/multiplecard/misc/UserConverterTest.java
@@ -0,0 +1,69 @@
+package com.thepapiok.multiplecard.misc;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.thepapiok.multiplecard.collections.Address;
+import com.thepapiok.multiplecard.collections.User;
+import com.thepapiok.multiplecard.dto.RegisterDTO;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class UserConverterTest {
+  private UserConverter userConverter;
+
+  @BeforeEach
+  public void setUp() {
+    userConverter = new UserConverter();
+  }
+
+  @Test
+  public void shouldSuccessGetEntityWithoutApartmentNumber() {
+    RegisterDTO registerDTO = new RegisterDTO();
+    registerDTO.setCity("City");
+    registerDTO.setStreet("Street");
+    registerDTO.setPostalCode("Postal Code");
+    registerDTO.setHouseNumber("1");
+    registerDTO.setCountry("Country");
+    registerDTO.setFirstName("First Name");
+    registerDTO.setLastName("Last Name");
+    registerDTO.setApartmentNumber("");
+    Address address = new Address();
+    address.setCity(registerDTO.getCity());
+    address.setStreet(registerDTO.getStreet());
+    address.setPostalCode(registerDTO.getPostalCode());
+    address.setHouseNumber(registerDTO.getHouseNumber());
+    address.setCountry(registerDTO.getCountry());
+    User expectedUser = new User();
+    expectedUser.setAddress(address);
+    expectedUser.setFirstName(registerDTO.getFirstName());
+    expectedUser.setLastName(registerDTO.getLastName());
+
+    assertEquals(expectedUser, userConverter.getEntity(registerDTO));
+  }
+
+  @Test
+  public void shouldSuccessGetEntity() {
+    RegisterDTO registerDTO = new RegisterDTO();
+    registerDTO.setCity("City");
+    registerDTO.setStreet("Street");
+    registerDTO.setPostalCode("Postal Code");
+    registerDTO.setHouseNumber("1");
+    registerDTO.setCountry("Country");
+    registerDTO.setFirstName("First Name");
+    registerDTO.setLastName("Last Name");
+    registerDTO.setApartmentNumber("1");
+    Address address = new Address();
+    address.setCity(registerDTO.getCity());
+    address.setStreet(registerDTO.getStreet());
+    address.setPostalCode(registerDTO.getPostalCode());
+    address.setHouseNumber(registerDTO.getHouseNumber());
+    address.setCountry(registerDTO.getCountry());
+    address.setApartmentNumber(1);
+    User expectedUser = new User();
+    expectedUser.setAddress(address);
+    expectedUser.setFirstName(registerDTO.getFirstName());
+    expectedUser.setLastName(registerDTO.getLastName());
+
+    assertEquals(expectedUser, userConverter.getEntity(registerDTO));
+  }
+}
