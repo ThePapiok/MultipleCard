6d3e4fe6f03464ef54e5d0bac36d1ede88eb9d3e
Adrian Politowski <politowskiadrian78@gmail.com>
2024-08-29 10:14:23 +0200

    Add responsive to login page

--- src/main/resources/static/js/loginPage.js
+++ src/main/resources/static/js/loginPage.js
@@ -9,36 +9,32 @@ let firstTime = [false, false];
 const regPassword = new RegExp("(?=.*[a-ząćęłńóśźż])(?=.*[A-ZĄĆĘŁŃÓŚŹŻ])(?=.*[0-9])(?=.*[!@#$%^&*])");
 const regPhone = new RegExp("^[1-9][0-9 ]*$")
 
-// TODO make unique this and css
 
-function check(i, con){
-    if(con){
-        ok[i-1] = true;
-        if(previous[i-1] === false){
-            document.getElementById("close"+i).style.display = "none";
-            document.getElementById("check"+i).style.display = "inline";
+function check(i, con) {
+    if (con) {
+        ok[i - 1] = true;
+        if (previous[i - 1] === false) {
+            document.getElementById("close" + i).style.display = "none";
+            document.getElementById("check" + i).style.display = "inline";
         }
-        if(ok[i-1] !== previous[i-1])
-        {
+        if (ok[i - 1] !== previous[i - 1]) {
             activeButton();
-            previous[i-1] = true;
+            previous[i - 1] = true;
         }
-    }
-    else{
-        ok[i-1] = false;
-        if(previous[i-1] === true){
-            document.getElementById("close"+i).style.display = "inline";
-            document.getElementById("check"+i).style.display = "none";
+    } else {
+        ok[i - 1] = false;
+        if (previous[i - 1] === true) {
+            document.getElementById("close" + i).style.display = "inline";
+            document.getElementById("check" + i).style.display = "none";
         }
-        if(ok[i-1] !== previous[i-1])
-        {
+        if (ok[i - 1] !== previous[i - 1]) {
             disableButton();
-            previous[i-1] = false;
+            previous[i - 1] = false;
         }
     }
 }
 
-function checkPhone(e){
+function checkPhone(e) {
     const input = e.value.toString().replaceAll(" ", "");
     phoneValue = input;
     setFullPhone();
@@ -46,25 +42,22 @@ function checkPhone(e){
     check(1, (input.length >= 7 && input.length + callingCodeLength <= 16 && regPhone.test(input)));
 }
 
-function  checkPassword(e){
+function checkPassword(e) {
     const input = e.value;
     check(2, (input.length >= 6 && input.length <= 25 && regPassword.test(input)));
 }
 
-function checkCallingCode(e){
+function checkCallingCode(e) {
     const input = e.value;
-    if(input === ''){
+    if (input === '') {
         ok[2] = false;
-        if(previous[2] !== ok[2])
-        {
+        if (previous[2] !== ok[2]) {
             disableButton();
             previous[2] = false;
         }
-    }
-    else{
+    } else {
         ok[2] = true;
-        if(previous[2] !== ok[2])
-        {
+        if (previous[2] !== ok[2]) {
             activeButton();
             previous[2] = false;
         }
@@ -72,8 +65,8 @@ function checkCallingCode(e){
     checkPhone(document.getElementById("phoneInput"));
 }
 
-function activeButton(){
-    if(ok[0] && ok[1] && ok[2]){
+function activeButton() {
+    if (ok[0] && ok[1] && ok[2]) {
         let button = document.getElementById("loginButton");
         button.className = "greenButton";
         button.type = "submit";
@@ -81,8 +74,8 @@ function activeButton(){
     }
 }
 
-function disableButton(){
-    if((!ok[0] || !ok[1] || !ok[2]) && success){
+function disableButton() {
+    if ((!ok[0] || !ok[1] || !ok[2]) && success) {
         let button = document.getElementById("loginButton");
         button.className = "grayButton";
         button.type = "button";
@@ -102,24 +95,24 @@ function hideValidation(e) {
     e.parentElement.nextElementSibling.style.display = "none";
 }
 
-function atStart(){
+function atStart() {
     checkCallingCode(document.getElementById("valueCallingCode"));
     checkPhone(document.getElementById("phoneInput"));
     document.getElementById("searchCallingCode").value = "";
 }
 
-function showOrHideCallingCode(){
-    callingCode=!callingCode;
+function showOrHideCallingCode() {
+    callingCode = !callingCode;
     let search = document.getElementById("searchCallingCode");
     let options = document.getElementById("optionsCallingCode");
     let up = document.getElementById("up1");
     let down = document.getElementById("down1");
-    if(callingCode){
+    if (callingCode) {
         search.style.display = "block";
         options.style.display = "block";
         up.style.display = "block";
         down.style.display = "none";
-    }else{
+    } else {
         search.style.display = "none";
         options.style.display = "none";
         up.style.display = "none";
@@ -127,38 +120,35 @@ function showOrHideCallingCode(){
     }
 }
 
-function searchAllCallingCodes(e){
+function searchAllCallingCodes(e) {
     let text = e.value.toString();
     const char = text.charAt(0);
     let options = document.getElementsByClassName("optionCallingCode");
     let nothing = document.getElementById("nothingCallingCode");
     let option;
     let isFound = false;
-    if(!isNaN(char)){
+    if (!isNaN(char)) {
         text = "+" + text;
-    }
-    else if(char !== '+'){
+    } else if (char !== '+') {
         text = text.toLowerCase();
     }
-    for(let i = 0; i < options.length; i++){
+    for (let i = 0; i < options.length; i++) {
         option = options[i];
-        if(option.textContent.startsWith(text) || option.dataset.code.toLowerCase().startsWith(text)){
+        if (option.textContent.startsWith(text) || option.dataset.code.toLowerCase().startsWith(text)) {
             option.style.display = "block";
             isFound = true;
-        }
-        else{
+        } else {
             option.style.display = "none";
         }
     }
-    if(!isFound){
+    if (!isFound) {
         nothing.style.display = "block";
-    }
-    else{
+    } else {
         nothing.style.display = "none";
     }
 }
 
-function setValueCallingCode(e){
+function setValueCallingCode(e) {
     let valueSelect = document.getElementById("valueCallingCode");
     valueSelect.value = e.dataset.value;
     checkCallingCode(valueSelect);
@@ -166,7 +156,7 @@ function setValueCallingCode(e){
     setFullPhone();
 }
 
-function setFullPhone(){
+function setFullPhone() {
     document.getElementById("fullPhone").value = callingCodeValue + phoneValue;
 }
 
