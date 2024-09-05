486073e2c4c58bd83a46cb38742ff90b0b97d6f0
Adrian Politowski <politowskiadrian78@gmail.com>
2024-09-05 22:57:51 +0200

    Spotless apply

--- src/main/java/com/thepapiok/multiplecard/MultiplecardApplication.java
+++ src/main/java/com/thepapiok/multiplecard/MultiplecardApplication.java
@@ -1,12 +1,9 @@
 package com.thepapiok.multiplecard;
 
-import com.mongodb.client.MongoClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Profile;
-import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.client.RestTemplate;
 
